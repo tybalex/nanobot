7b55f388a352faa340cef9f5810daa913db7e4d0
Yingbei <yingbei@acorn.io>
2025-06-04 16:27:30 -0700

    date time

--- examples/myresearch-agent/agent/mcp_server.py
+++ examples/myresearch-agent/agent/mcp_server.py
@@ -1,25 +1,23 @@
 # server.py
+from datetime import datetime
 from mcp.server.fastmcp import FastMCP
 from agent.graph import graph
 
 # Create an MCP server
-mcp = FastMCP("Demo")
+mcp = FastMCP("MyResearch")
 
-
-# Add an addition tool
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
-
-
+# https://github.com/google-gemini/gemini-fullstack-langgraph-quickstart/tree/main
 @mcp.tool()
 def web_research(query: str) -> str:
-    """Search the web for information"""
+    """Search the web for information, do some research """
     state = graph.invoke({"messages": [{"role": "user", "content": query}], "max_research_loops": 3, "initial_search_query_count": 3})
-    # content = markdownify(state["messages"][-1].content)
     return state['messages'][-1].content
 
+
+@mcp.tool()
+def get_date_time(query: str) -> str:
+    """Get the current date and time"""
+    return f"The current date and time is {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 
 if __name__ == "__main__":
     mcp.run()
