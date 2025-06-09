9ebd414114995486e98471551835d3d78a96b750
Bill Maxwell <cloudnautique@users.noreply.github.com>
2025-06-09 16:37:23 -0700

    chore: fix uri matching for resource templates
    Signed-off-by: Bill Maxwell <cloudnautique@users.noreply.github.com>

--- pkg/server/uritemplate.go
+++ pkg/server/uritemplate.go
@@ -1,15 +1,49 @@
 package server
 
 import (
+	"fmt"
 	"regexp"
+	"strings"
 )
 
-var uriTemplateRegex = regexp.MustCompile(`\{([^/]+?)}`)
-
 func uriToRegexp(template string) (*regexp.Regexp, error) {
-	// This is naive and stupid and doesn't work right because it doesn't escape the regexp
-	reStr := uriTemplateRegex.ReplaceAllString(template, `([^/]+)`)
-	reStr = "^" + reStr + "$"
+	// Build regex by processing the template character by character
+	var result strings.Builder
+	result.WriteByte('^')
 
-	return regexp.Compile(reStr)
+	i := 0
+	for i < len(template) {
+		if template[i] == '{' {
+			// Find the end of the template variable
+			end := strings.Index(template[i:], "}")
+			if end == -1 {
+				return nil, fmt.Errorf("unclosed template variable in %q", template)
+			}
+			end += i
+
+			// Extract parameter name
+			param := template[i+1 : end]
+
+			// Convert parameter to regex pattern
+			if strings.HasPrefix(param, "/") && strings.HasSuffix(param, "*") {
+				// {/path*} - optional path segment that can contain slashes
+				result.WriteString("(/.*?)?")
+			} else if strings.HasSuffix(param, "*") {
+				// {param*} - parameter that can contain slashes
+				result.WriteString("(.*?)")
+			} else {
+				// {param} - regular parameter that cannot contain slashes
+				result.WriteString("([^/]+?)")
+			}
+
+			i = end + 1
+		} else {
+			// Escape literal character for regex
+			result.WriteString(regexp.QuoteMeta(string(template[i])))
+			i++
+		}
+	}
+
+	result.WriteByte('$')
+	return regexp.Compile(result.String())
 }
