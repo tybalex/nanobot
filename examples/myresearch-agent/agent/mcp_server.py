4d164de11bf1cc9208f9c9b2df063e4a9f03177e
Yingbei <yingbei@acorn.io>
2025-06-10 18:40:19 -0700

    minor update

--- examples/myresearch-agent/agent/mcp_server.py
+++ examples/myresearch-agent/agent/mcp_server.py
@@ -10,12 +10,12 @@ mcp = FastMCP("MyResearch")
 @mcp.tool()
 def web_research(query: str) -> str:
     """Search the web for information, do some research """
-    state = graph.invoke({"messages": [{"role": "user", "content": query}], "max_research_loops": 3, "initial_search_query_count": 3})
+    state = graph.invoke({"messages": [{"role": "user", "content": f"Today's date and time: {get_date_time()}\nUser question: {query}"}], "max_research_loops": 3, "initial_search_query_count": 3})
     return state['messages'][-1].content
 
 
 @mcp.tool()
-def get_date_time(query: str) -> str:
+def get_date_time() -> str:
     """Get the current date and time"""
     return f"The current date and time is {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 
